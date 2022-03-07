a69a2228864b42b2bc42ff4a971da7135a415348
shaoshitong <1090784053@qq.com>
2022-03-07 23:45:23 +0800

    1

--- Transfer/Transfer.py
+++ Transfer/Transfer.py
@@ -33,7 +33,10 @@ class ReverseLayerF(torch.autograd.Function):
         output = grad_output.clone()
         return output, None
 
-
+def normalize(feature,eps):
+    norm=torch.norm(feature,2,1,keepdim=True)+eps
+    feature=torch.div(feature,norm)
+    return feature
 class Multi_GRL(nn.Module):
     def __init__(self, extractor, in_channels, subject_nums, num_classes):
         super(Multi_GRL, self).__init__()
@@ -50,7 +53,8 @@ class Multi_GRL(nn.Module):
             nn.ReLU(inplace=True),
             nn.Linear(1024, subject_nums)
         ])
-        self.betas=0.9
+        self.betas = 0.5
+
     def forward(self, x, y, subject_mask, subject_label, subject):
         """
         :param x: 输入样本
@@ -70,17 +74,19 @@ class Multi_GRL(nn.Module):
             subject_mask = subject_mask - subject_mask.min(dim=-1, keepdim=True)[0]
             # subject_mask = rearrange(subject_mask, "(s i) v -> s i v", s=subject_mask.shape[1]).mean(1)
             subject_mask = (subject_mask.max(dim=-1, keepdim=True)[0] - subject_mask)
-            subject_mask = subject_mask/subject_mask.sum(-1,keepdim=True)
-            yin_subject_mask=torch.eye(subject_mask.shape[1],dtype=torch.float).to(subject_mask.device).unsqueeze(1).repeat(1,int(subject_mask.shape[0]/subject_mask.shape[1]),1).view(-1,subject_mask.shape[-1])
-            subject_mask=self.betas*subject_mask+(1-self.betas)*yin_subject_mask
-            loss_subject = F.mse_loss(subject_out, subject_mask, size_average=False, reduction="none").sum(-1).mean()
+            subject_mask = subject_mask / subject_mask.sum(-1, keepdim=True)
+            yin_subject_mask = torch.eye(subject_mask.shape[1], dtype=torch.float).to(subject_mask.device).unsqueeze(
+                1).repeat(1, int(subject_mask.shape[0] / subject_mask.shape[1]), 1).view(-1, subject_mask.shape[-1])
+            loss_subject_1 = F.mse_loss(subject_out, subject_mask,size_average=False, reduction="none")
+            loss_subject_2 = F.mse_loss(subject_out, yin_subject_mask, size_average=False,reduction="none")
+            loss_subject = self.betas * loss_subject_1 + (1. - self.betas) * loss_subject_2
             use = ~torch.eq(torch.LongTensor([subject]).to(subject_label.device), subject_label)
             y = y.long()[use]
             label_out = label_out[use]
             loss_class = crossentropy()(label_out, y)
             pred = torch.argmax(label_out, dim=1)
             correct = 100 * torch.eq(pred, y.argmax(1)).float().sum().item() / pred.shape[0]
-            return loss_class, loss_subject * 0.5, correct
+            return loss_class, loss_subject*0.5, correct
         else:
             x = self.extractor(x)
             label_out = self.class_predictor(x)
